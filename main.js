e184408eeee95c20be5eda243133807c73b425cc
wbrunovieira <wbrunovieira77@gmail.com>
2023-12-22 09:03:00 -0300

    added white background and interections

--- main.js
+++ main.js
@@ -2,11 +2,45 @@ import * as THREE from 'three';
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff); 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+function onMouseClick(event) {
+    // Calcular a posição do mouse relativa à cena
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+
+    // Atualizar o raycaster com a posição do mouse
+    raycaster.setFromCamera(mouse, camera);
+
+    // Calcular objetos que estão intersectando o raio lançado
+    const intersects = raycaster.intersectObjects(scene.children);
+
+    if (intersects.length > 0) {
+
+      if (intersects[0].object === textMesh) {
+        // Mova ou altere o textMesh como desejado
+        textMesh.position.x += 1; // Exemplo: move o texto para a direita
+    }
+        // intersects[0].object é o objeto mais próximo que foi clicado
+        // Aqui, você pode adicionar a lógica para manipular o objeto, por exemplo:
+        console.log('Objeto clicado:', intersects[0].object);
+    }
+}
+
+window.addEventListener('click', onMouseClick);
 
 const light = new THREE.AmbientLight(0xffffff); // luz branca suave
 scene.add(light);
@@ -20,6 +54,8 @@ loader.load('/fonts/SourceCodePro_Bold.json', function (font) {
         size: 1,
         height: 0.2
     });
+
+    textGeometry.center();
 
     const textMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 }); // Vermelho
     textMesh = new THREE.Mesh(textGeometry, textMaterial);
